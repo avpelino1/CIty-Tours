e00355ed967c3d36a313e843956de5f58e945fa7
Alisa Pelino <avpelino1@gmail.com>
2020-10-05 18:21:52 -0400

    added method headers for the controllers

--- java/src/main/java/com/techelevator/controller/LandmarkController.java
+++ java/src/main/java/com/techelevator/controller/LandmarkController.java
@@ -1,8 +1,28 @@
 package com.techelevator.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LandmarkController {
 
+	@RequestMapping(value="/landmarks/all", method=RequestMethod.GET)
+	public void allLandmarks() {}
+	
+	@RequestMapping(value="/landmarks/:id", method=RequestMethod.GET)
+	public void landmarkDetails() {}
+	
+	@RequestMapping(value="/landmarks/add", method=RequestMethod.POST)
+	public void addLandmark() {}
+	
+	@RequestMapping(value="/landmarks/pending", method=RequestMethod.GET)
+	public void pendingLandmarks() {}
+	
+	@RequestMapping(value="/landmarks/pending/:id/approve", method=RequestMethod.PUT)
+	public void approvePendingLandmark() {}
+	
+	@RequestMapping(value="/landmarks/pending/:id/deny", method=RequestMethod.DELETE)
+	public void denyPendingLandmark() {}
+	
 }
