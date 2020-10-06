c900333b7c8db57df6121728f8c8eddaf8b25239
g-zhao <zhao.gordon@gmail.com>
2020-10-06 17:02:48 -0500

    resolved @component in JDCB classes

--- java/src/main/java/com/techelevator/dao/ItineraryDAO.java
+++ java/src/main/java/com/techelevator/dao/ItineraryDAO.java
@@ -2,6 +2,8 @@ package com.techelevator.dao;
 
 import java.util.List;
 
+import org.springframework.stereotype.Component;
+
 import com.techelevator.model.Itinerary;
 import com.techelevator.model.Landmark;
 
--- java/src/main/java/com/techelevator/dao/LandmarkDAO.java
+++ java/src/main/java/com/techelevator/dao/LandmarkDAO.java
@@ -2,6 +2,8 @@ package com.techelevator.dao;
 
 import java.util.List;
 
+import org.springframework.stereotype.Component;
+
 import com.techelevator.model.Landmark;
 
 public interface LandmarkDAO {
