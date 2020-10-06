1aa6c3c14f4d13e42821238b53b1a10a615e591f
g-zhao <zhao.gordon@gmail.com>
2020-10-06 14:48:46 -0500

    progess for JDBCLandmarkDAO.java

---
+++ java/src/main/java/com/techelevator/model/Images.java
@@ -0,0 +1,26 @@
+package com.techelevator.model;
+
+public class Images {
+	private Long landmarkId;
+	private Long reviewId;
+	private String imgUrl;
+	public Long getLandmarkId() {
+		return landmarkId;
+	}
+	public void setLandmarkId(Long landmarkId) {
+		this.landmarkId = landmarkId;
+	}
+	public Long getReviewId() {
+		return reviewId;
+	}
+	public void setReviewId(Long reviewId) {
+		this.reviewId = reviewId;
+	}
+	public String getImgUrl() {
+		return imgUrl;
+	}
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+	
+}
