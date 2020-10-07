8b94db7a311fb9a5f0eb0be4461174a447a55e61
Alisa Pelino <avpelino1@gmail.com>
2020-10-07 10:20:22 -0400

    user controller method added

--- java/src/main/java/com/techelevator/controller/UserController.java
+++ java/src/main/java/com/techelevator/controller/UserController.java
@@ -1,8 +1,24 @@
 package com.techelevator.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.techelevator.dao.UserDAO;
+import com.techelevator.model.User;
 
 @RestController
 public class UserController {
+	
+	@Autowired
+	UserDAO userDAO;
+	
+	@RequestMapping(path="/users", method=RequestMethod.GET)
+	public List<User> retrieveAll () {
+		return userDAO.findAll();
+	}
 
 }
