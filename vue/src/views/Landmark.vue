e712bfbcee58424f07b5bd9cabcf99a70505ea08
Kaylee Anchulis <kayleeanchulis@gmail.com>
2020-10-07 15:11:55 -0500

    pushing vue stuff

---
+++ vue/src/views/Landmark.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="landmark" class="text-center">
+       <h1 class="h3 mb-3 font-weight-normal">Explore Landmarks in Austin</h1>
+
+
+
+      
+  </div>
+</template>
+
+<script>
+import authService from '../services/AuthService';
+
+export default {
+    name: 'landmark',
+
+}
+</script>
+
+
+<style>
+
+</style>
