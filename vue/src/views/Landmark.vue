a8b2bf003ebc1f21897f55537dc4e27d7d9739e4
Jacob Maniaci <jacobmaniaci@outlook.com>
2020-10-08 11:27:45 -0400

    update

--- vue/src/views/Landmark.vue
+++ vue/src/views/Landmark.vue
@@ -1,9 +1,8 @@
 <template>
   <div id="landmark" class="text-center">
        <h1 class="h3 mb-3 font-weight-normal">Explore Landmarks in Austin</h1>
-         <div v-for = 'landmark in landmarks' v-bind:key='landmark.id'>
           <landmark-map />
-         </div>
+         
 
 
       
