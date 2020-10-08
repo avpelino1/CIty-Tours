7b71af164c099445c129ad6c63b4a9899ec81578
Alisa Pelino <avpelino1@gmail.com>
2020-10-08 14:01:34 -0400

    added to landmark view

--- vue/src/components/LandmarkList.vue
+++ vue/src/components/LandmarkList.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <h1>List of Landmarks</h1>
-    <div v-for = 'landmark in landmarks' v-bind:key='landmark.id'/>
+    <div class='landmark-list' v-for='landmark in landmarks' v-bind:key='landmark.id'>
+      <p>hello world</p>
+      {{landmark.name}} <br>
+      {{landmark.description}} <br>
+    </div>
 
     <h2>Want to add a landmark? Fill out this form!</h2>
     <form v-on:submit.prevent>
@@ -61,4 +65,11 @@ export default {
 </script>
 
 <style>
+    .landmark-list{
+        padding: 5px;
+        margin: 5px;
+        border: 1px;
+        width: 250px;
+        background-color: lavender;
+    }
 </style>
