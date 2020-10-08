666f5fb733b303b396fbdc4748b9fb0eda300df1
Alisa Pelino <avpelino1@gmail.com>
2020-10-07 21:34:09 -0400

    added router links between views, wrote skeleton code for Itinerary.vue

---
+++ vue/src/services/ItineraryService.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const http = axios.create({
+    baseURL: "http://localhost:8080"
+});
+
+export default {
+    getItineraries(){
+        return http.get('/itinerary');
+    },
+   
+    createItinerary(itinerary) {
+        return axios.post('/login', itinerary)
+    },
+
+    }
---
+++ vue/src/views/Itinerary.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class='itinerary-list'>
+      <div v-for='itinerary in itineraries' v-bind:key='itinerary.id' class='itinerary'>
+          <!--
+            <router-link :to='{ name: 'ItineraryDetails'}'> View Itinerary Details <router-link>
+            -->
+    </div>
+   </div>
+</template>
+
+<script>
+import ItineraryService from '../services/ItineraryService.js';
+// import ItineraryDetails from './views/ItineraryDetails.vue';
+
+export default {
+    name: 'itinerary-list',
+    data(){
+        return{
+            itineraries: []
+        }
+    },
+    created(){
+        ItineraryService.getItineraries().then((response)=>{
+            this.itineraries = response.data;
+        });
+    }
+}
+</script>
+
+<style>
+.topic-list {
+  padding: 20px 20px;
+  margin: 0 auto;
+  max-width: 600px;
+}
+.topic {
+  font-size: 24px;
+  border-bottom: 1px solid #f2f2f2;
+  padding: 10px 20px;
+}
+.topic:last-child {
+  border: 0px;
+}
+</style>
