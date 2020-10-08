42cd847113c76bb14e6c8a936ec088cd3fbf666a
g-zhao <zhao.gordon@gmail.com>
2020-10-08 14:29:40 -0500

    pushing updates with @CrossOrigin

--- java/src/main/java/com/techelevator/controller/ItineraryController.java
+++ java/src/main/java/com/techelevator/controller/ItineraryController.java
@@ -3,6 +3,7 @@ package com.techelevator.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,30 +13,32 @@ import org.springframework.web.bind.annotation.RestController;
 import com.techelevator.dao.ItineraryDAO;
 import com.techelevator.model.Itinerary;
 
+@CrossOrigin
 @RestController
+@RequestMapping("/itinerary")
 public class ItineraryController {
 	
 	@Autowired
 	ItineraryDAO itineraryDAO;
 	
-	@RequestMapping(path="/itinerary", method=RequestMethod.GET)
+	@RequestMapping(path="", method=RequestMethod.GET)
 	public List<Itinerary> retrieveAllItinerary() {
-		return itineraryDAO.retrieveAllItinerary();
+		return itineraryDAO.retrieveAllUserItinerary();
 	}
 	
 	
 
-	@RequestMapping(path="/itinerary/create", method=RequestMethod.POST)
+	@RequestMapping(path="/create", method=RequestMethod.POST)
 	public void createItinerary(@RequestBody Itinerary itinerary) {
 		itineraryDAO.createItinerary(itinerary);
 	}
 	
-	@RequestMapping(path="/itinerary/{id}/update", method=RequestMethod.PUT)
+	@RequestMapping(path="/{id}/update", method=RequestMethod.PUT)
 	public void updateItinerary(@RequestBody Itinerary newItinerary, @PathVariable int id) {
 		itineraryDAO.updateItinerary(newItinerary, id);
 	}
 	
-	@RequestMapping(path="/itinerary/{id}/delete", method=RequestMethod.DELETE)
+	@RequestMapping(path="/{id}/delete", method=RequestMethod.DELETE)
 	public void deleteItinerary(@PathVariable int id) {
 		itineraryDAO.deleteItinerary(id);
 	}
