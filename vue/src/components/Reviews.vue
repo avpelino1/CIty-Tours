e301368af19f069ce0cb56d14453393537e82acf
Kaylee Anchulis <kayleeanchulis@gmail.com>
2020-10-12 15:17:49 -0500

    reviewService added

---
+++ vue/src/components/Reviews.vue
@@ -0,0 +1,44 @@
+<template>
+<div>
+
+
+  <h3> Add a Review </h3>
+
+
+      </div> 
+</template>
+
+<script>
+import reviewService from '../services/ReviewService.js';
+
+export default {
+    name: 'reviews',
+    data() {
+        return {
+            newReview: {
+    
+            }
+
+        }
+
+    },
+
+    methods: {
+        addReview() {
+            reviewService.createReview(this.newReview);
+        },
+        getReviews(id) {
+            reviewService.getReviews(id);
+        }
+
+    },
+
+    created() {
+
+    }
+}
+</script>
+
+<style>
+
+</style>
