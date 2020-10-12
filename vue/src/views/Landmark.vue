673944dca031ca9e7c362d9acf0325e9e9a72c32
Jacob Maniaci <jacobmaniaci@outlook.com>
2020-10-12 15:47:58 -0400

    update

--- vue/src/views/Landmark.vue
+++ vue/src/views/Landmark.vue
@@ -2,11 +2,6 @@
   <div id="landmark" class="text-center">
        <h1 class="h3 mb-3 font-weight-normal">Explore Landmarks in Austin, TX</h1>
 
-<<<<<<< HEAD
-
-=======
-<GoogleMapLoader class="GoogleMapLoader"/>
->>>>>>> af28a147098a67fea51cdcd8f24272c7a1a7cc5b
 <landmark-list/>
 <landmark-form/>
       
@@ -15,21 +10,13 @@
 
 <script>
 import LandmarkList from '@/components/LandmarkList.vue';
-<<<<<<< HEAD
-=======
-import GoogleMapLoader from '@/components/GoogleMapLoader.vue';
->>>>>>> af28a147098a67fea51cdcd8f24272c7a1a7cc5b
 import LandmarkForm from '@/components/LandmarkForm.vue';
 
 export default {
     name: 'landmark',
     components: {
-      GoogleMapLoader,
+      //GoogleMapLoader,
       LandmarkList,
-<<<<<<< HEAD
-
-=======
->>>>>>> af28a147098a67fea51cdcd8f24272c7a1a7cc5b
       LandmarkForm
     }
 }
