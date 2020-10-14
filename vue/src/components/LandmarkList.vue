94e0b7888437824a54a165a3fba54c364cd45b6b
Kaylee Anchulis <kayleeanchulis@gmail.com>
2020-10-14 10:22:49 -0500

    idk

--- vue/src/components/LandmarkList.vue
+++ vue/src/components/LandmarkList.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
+
+    <form v-on:submit.prevent='updateItinerary'>
+      <label name ='selectItinerary'>Select Itinerary: </label> 
+      <select v-model="updatedItinerary.name">
+        <option v-for="itinerary in itineraries" v-bind:key='itinerary.itineraryId'> {{itinerary.name}} </option>
+      </select> <br> <br>
+
+      <div class='actions'>
+    <button>Click here to update your itinerary!</button> <br>
+      </div>
+
     <div class='landmark-list' v-for='landmark in landmarks' v-bind:key='landmark.id'>
       <router-link v-bind:to="{name: 'landmark-details', params: {id: landmark.id} }">
           <h3 class='sub-header'>{{landmark.name}}</h3> <br>
@@ -11,6 +22,7 @@
       <br><br>
       <input type="checkbox" v-on:click="addLandmark(landmark.id)"/><label name='addToItinerary'> Add to Itinerary </label>
     </div>
+    </form>
 
   </div>
 </template>
@@ -65,14 +77,15 @@ export default {
   created() {
     landmarkService.getLandmarks().then((response)=>{
       this.landmarks=response.data;
-    })
+    }
+     )
   },
 
-  computed: {
-    filterSearch() {
-      return this.landmarks.filter
-    }
-  }
+  // computed: {
+  //   filterSearch() {
+  //     return this.landmarks.filter        // Need to be able to filter by venue type, name, etc.
+  //   }
+  // }
 
 }
 
