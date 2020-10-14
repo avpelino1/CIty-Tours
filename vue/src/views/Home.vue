cb71d5641d57883dbca146bc1a0a1d9e64147621
g-zhao <zhao.gordon@gmail.com>
2020-10-14 13:19:10 -0500

    merge

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <!-- <googleMaps /> -->
+    <googleMaps />
     <router-link :to="{ name: 'landmark'}"> View a List of Landmarks in Austin, TX  </router-link><br><br>
     <router-link :to="{ name: 'itinerary'}"> View or Create an Itinerary </router-link>
   </div>
 </template>
 
 <script>
-// import GoogleMaps from '@/components/GoogleMaps.vue'
+import GoogleMaps from '@/components/GoogleMaps.vue'
 
 export default {
-  name: "home"
+  name: "home",
+  GoogleMaps
 };
 </script>
