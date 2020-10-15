c53d76e6df9dadd5c5c5eacdc7302d34b8e2f1d7
Alisa Pelino <avpelino1@gmail.com>
2020-10-15 16:41:12 -0400

    header changes

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,19 +1,24 @@
 <template>
   <div class="home">
     <div class="header">
-      <h1>City Tour of Austin, TX</h1>
-      <h4>Welcome, {{this.$store.state.user.username}}!</h4>
-    </div>
+      <div id='header-foreground'>
+        <h1 id='header-words'>City Tour of Austin, TX</h1>
+        <h4 id='header-words'>Welcome, {{this.$store.state.user.username}}!</h4>
+      </div>
     <div class="container">
-      <router-link :to="{ name: 'landmark'}"> View a List of Landmarks in Austin, TX  </router-link>
-      <router-link :to="{ name: 'landmark'}"> Create a New Itinerary  </router-link>
-      <router-link :to="{ name: 'itinerary'}"> View or Edit Your Existing Itineraries </router-link>
+      <router-link class='link' :to="{ name: 'landmark'}"> View a List of Landmarks in Austin, TX  </router-link>
+      <router-link class='link' :to="{ name: 'landmark'}"> Create a New Itinerary  </router-link>
+      <router-link class='link' :to="{ name: 'itinerary'}"> View or Edit Your Existing Itineraries </router-link>
+    </div>
     </div>
     
-    <img id='skyline' src='https://aquilacommercial.com/wp-content/uploads/2019/05/city-of-austin-skyline.jpg'/>
-
-    <h4>The city of Austin is a beautiful place, full of diversity. Regardless of which landmarks you choose to visit, you are sure to have a wonderful time!</h4>
-    <h3 class='weird'>Keep Austin Weird!</h3>
+    <div class='home-description'>
+      <div class='blurb'>
+      <h4>The city of Austin is a beautiful place, full of diversity. Regardless of which landmarks you choose to visit, you are sure to have a wonderful time!</h4>
+      <h3 class='weird'>Keep Austin Weird!</h3>
+      </div>
+    <img class='skyline' src='https://storage.needpix.com/rsynced_images/austin-4064427_1280.jpg'/>
+    </div>
 
   </div>
 </template>
@@ -44,20 +49,49 @@ export default {
     padding-top: 5px;
     padding-bottom: 20px;
     text-align: center;
-    background: lavender;
+    color: navy;
+    background-image: url("https://aquilacommercial.com/wp-content/uploads/2019/05/city-of-austin-skyline.jpg"); /* The image used */
+    background-color: #cccccc; /* Used if the image is unavailable */
+    height: 250px; /* You must set a specified height */
+    background-position: center; /* Center the image */
+    background-repeat: no-repeat; /* Do not repeat the image */
+    background-size: cover; /* Resize the background image to cover the entire container */
+    border-radius: 20px;
+
+  }
+  #header-foreground{
+    background-color:lavenderblush;
+    opacity: 0.75;
+    border-radius: 700px;
+    margin-left: 700px;
+    margin-right: 700px;
+    margin-top: 50px;
+    margin-bottom: 25px;
+    padding-bottom: 5px;
+  }
+  #header-words{
+    opacity: 1;
     color: navy;
   }
   .container {
     display: flex;
     flex-grow: 3;
     justify-content: space-between;
-    background: lavender;
-    padding-bottom: 10px;
+    color:white;
+    border-radius: 0px;
+    padding-top: 12px;
     padding-left: 10px;
-    padding-right: 10px;
+    padding-right: 15px;
+    padding-bottom: 5px;
+    margin-bottom: 5px;
   }
-  #skyline{
-    width: 100%;
+  .skyline{
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: 50%;
+    border-radius: 100px;
+    margin-top: 30px;
   }
   .weird{
     font-style: italic;
@@ -65,5 +99,29 @@ export default {
   }
   .home {
      font-family: 'Ubuntu', sans-serif;
+  }
+  .link{
+    background-color:lavenderblush;
+    opacity: 0.75;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    padding-right: 15px;
+    padding-left: 15px;
+    margin-bottom: 5px;
+    border-radius: 40px;
+  }
+  .home-description{
+    display: grid;
+    grid-template-columns: auto auto;
+    grid-template-areas:
+      "blurb skyline"
+      ;
+    margin-top: 50px;
+  }
+  .blurb{
+    padding: 50px;
+    margin: 50px;
+    background-image: linear-gradient(to bottom right, lightblue, lavender);
+    border-radius: 50px;
   }
 </style>
